9aec191b3e24ac7535f9f83a129facae680968f5
janish-parikh <anshji666@gmail.com>
2022-09-14 07:23:18 -0400

    Fixed Spelling

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,7 +59,7 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Jniash Parikh.</h2>;
+  const two = <h2 className="big-heading">Janish Parikh.</h2>;
   const three = <h3 className="big-heading">I am Data Scienitst.</h3>;
   const four = (
     <>
