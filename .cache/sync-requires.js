70eb6b5273c699cbf13ac0afa103de78df23a757
janish-parikh <anshji666@gmail.com>
2022-09-14 06:41:53 -0400

    Added changes after npm build

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,13 +4,13 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/vamshisaggurthi/Downloads/Devlopment/Personal website/personal_website/.cache/dev-404-page.js")),
-  "component---src-pages-404-js": preferDefault(require("/Users/vamshisaggurthi/Downloads/Devlopment/Personal website/personal_website/src/pages/404.js")),
-  "component---src-pages-archive-js": preferDefault(require("/Users/vamshisaggurthi/Downloads/Devlopment/Personal website/personal_website/src/pages/archive.js")),
-  "component---src-pages-index-js": preferDefault(require("/Users/vamshisaggurthi/Downloads/Devlopment/Personal website/personal_website/src/pages/index.js")),
-  "component---src-pages-pensieve-index-js": preferDefault(require("/Users/vamshisaggurthi/Downloads/Devlopment/Personal website/personal_website/src/pages/pensieve/index.js")),
-  "component---src-pages-pensieve-tags-js": preferDefault(require("/Users/vamshisaggurthi/Downloads/Devlopment/Personal website/personal_website/src/pages/pensieve/tags.js")),
-  "component---src-templates-post-js": preferDefault(require("/Users/vamshisaggurthi/Downloads/Devlopment/Personal website/personal_website/src/templates/post.js")),
-  "component---src-templates-tag-js": preferDefault(require("/Users/vamshisaggurthi/Downloads/Devlopment/Personal website/personal_website/src/templates/tag.js"))
+  "component---cache-caches-gatsby-plugin-offline-app-shell-js": preferDefault(require("/Users/janishparikh/personal_website/.cache/caches/gatsby-plugin-offline/app-shell.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/janishparikh/personal_website/src/pages/404.js")),
+  "component---src-pages-archive-js": preferDefault(require("/Users/janishparikh/personal_website/src/pages/archive.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/janishparikh/personal_website/src/pages/index.js")),
+  "component---src-pages-pensieve-index-js": preferDefault(require("/Users/janishparikh/personal_website/src/pages/pensieve/index.js")),
+  "component---src-pages-pensieve-tags-js": preferDefault(require("/Users/janishparikh/personal_website/src/pages/pensieve/tags.js")),
+  "component---src-templates-post-js": preferDefault(require("/Users/janishparikh/personal_website/src/templates/post.js")),
+  "component---src-templates-tag-js": preferDefault(require("/Users/janishparikh/personal_website/src/templates/tag.js"))
 }
 
